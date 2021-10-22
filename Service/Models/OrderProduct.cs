026eeefb4d596d0affdfe67fadb595cb72d71862
vivien.clifford <vivien.clifford18@gmail.com>
2021-10-22 23:25:42 +1100

    Create models for the tests

---
+++ Service/Models/OrderProduct.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Service.Models
+{
+    public class OrderProduct
+    {
+        public List<Product> Products { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
