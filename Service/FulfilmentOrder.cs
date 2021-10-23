a82c8b07db8d1ced8d0ffe617cfead2b614d8070
vivien.clifford <vivien.clifford18@gmail.com>
2021-10-23 17:08:50 +1100

    Rewriting fulfilment order process

--- Service/FulfilmentOrder.cs
+++ Service/FulfilmentOrder.cs
@@ -32,57 +32,12 @@ namespace Service
             return statusOrder;
         }
 
-        #region "Order Processing"
-
-        public List<OrderItemAvailability> CalculateStockAvailability()
+        public IEnumerable<OrderProduct> CalculateStockAvailability()
         {
             var pendingOrders = GetOrdersByStatus(nameof(OrderStatusEnum.Pending));
-            List<OrderItemAvailability> orderItemsAvailability = new();
 
-            if (pendingOrders != null)
-                orderItemsAvailability = CheckCurrentStock(pendingOrders); //whether individual stocks can be fufilled
+            return OrderProcessing.ProcessOrders(pendingOrders);
 
-            return orderItemsAvailability;
         }
-
-        /// <summary>
-        /// Checks for the order items can be fulfiled and marks individual items as can/can't
-        /// Decrements the stock availablity as we go, so it's always up to date.
-        /// </summary>
-        /// <param name="pendingOrders"></param>
-        /// <returns></returns>
-        private static List<OrderItemAvailability> CheckCurrentStock(IEnumerable<OrderProduct> pendingOrders)
-        {
-            List<Product> products = pendingOrders.FirstOrDefault().Products;
-            List<Order> orders = pendingOrders.FirstOrDefault().Orders; //TO DO might have multiple orders too
-
-            List<OrderItemAvailability> orderItemsAvailability = new();
-
-            foreach (var order in orders)
-            {
-                foreach (var item in order.Items)
-                {
-                    var stockAvailable = IsStockAvailable(products, item);
-
-                    orderItemsAvailability.Add(new OrderItemAvailability(item.OrderId, item.ProductId, stockAvailable));
-                    DecrementStock(item);
-                }
-            }
-            return orderItemsAvailability;
-        }
-
-        private static bool IsStockAvailable(IEnumerable<Product> products, Item item)
-        {
-            var stockProduct = products.Single(p => p.ProductId == item.ProductId);
-
-            return (stockProduct.QuantityOnHand >= item.Quantity);
-        }
-
-        private static void DecrementStock(Item item) // TO DO
-        {
-            //var stockProduct = fulfilmentOrder.GetOrdersByStatus("pending").Select(p => p.Products);
-        }
-
-        #endregion "Order Processing"
     }
 }
---
+++ Service/OrderProcessing.cs
@@ -0,0 +1,72 @@
+﻿using Service.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Service
+{
+    public class OrderProcessing
+    {
+        /// <summary>
+        /// Checks for the order items can be fulfiled
+        /// </summary>
+        /// <param name="pendingOrders"></param>
+        /// <returns></returns>
+        public static IEnumerable<OrderProduct> ProcessOrders(IEnumerable<OrderProduct> pendingOrders)
+        {
+            List<Product> products = pendingOrders.FirstOrDefault().Products;
+            List<Order> orders = pendingOrders.FirstOrDefault().Orders; //TO DO might have multiple orders too
+
+            foreach (var order in orders)
+            {
+                var updatedProducts = UpdateProductQuantity(products, order);
+
+                if (updatedProducts != null)
+                {
+                    //order has been fulfilled
+                    products = updatedProducts;
+                    order.Status = nameof(OrderStatusEnum.Fulfilled); //need to test
+                }
+                else
+                {
+                    order.Status = nameof(OrderStatusEnum.Unfulfillable); //need to test, need to raise error
+                }
+
+            }
+
+            return new List<OrderProduct>() { new OrderProduct(products, orders) };
+        }
+
+        /// <summary>
+        ///  Decrements the stock availablity as we go, so the product list always up to date.
+        ///  Calls ThresholdCalculator to check if we need to restock items / create a purchase order
+        /// </summary>
+        /// <param name="products"></param>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        private static List<Product> UpdateProductQuantity(List<Product> products, Order order)
+        {
+            var updatedProducts = products;
+
+            foreach (var item in order.Items)
+            {
+                var stockProduct = updatedProducts.Single(p => p.ProductId == item.ProductId);
+                var itemQuantityAvailable = (stockProduct.QuantityOnHand >= item.Quantity);
+
+                //we need to calculate whether the min threshold has been reached
+                Restock.ThresholdCalculator(stockProduct, order);
+
+                if (itemQuantityAvailable)
+                {
+                    stockProduct.QuantityOnHand -= item.Quantity;
+                }
+                else
+                {
+                    return null; //TO DO - can't fulfil order
+                }
+
+            }
+
+            return updatedProducts;
+        }
+    }
+}
