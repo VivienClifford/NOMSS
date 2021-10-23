e493d3c6555550dd439c457a6d5886b7fb48c4e8
vivien.clifford <vivien.clifford18@gmail.com>
2021-10-23 11:48:32 +1100

    Configured sample data source, added necessary methods to start writing tests

---
+++ Service/FulfilmentOrder.cs
@@ -0,0 +1,26 @@
+﻿using Service.Helper;
+using Service.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Service
+{
+    public class FulfilmentOrder : IFulfilmentOrder
+    {
+        public OrderProduct GetOrders()
+        {
+            var orderProduct = JsonDeserializer.GetData();
+
+            return orderProduct;
+        }
+
+        public IEnumerable<OrderProduct> GetOrdersByStatus(string status)
+        {
+            var orders = GetOrders()
+                         .Orders.Where(o => o.Status == status)
+                         .Select(o => o);
+
+            return (IEnumerable<OrderProduct>)orders;
+        }
+    }
+}
---
+++ Service/IFulfilmentOrder.cs
@@ -0,0 +1,20 @@
+﻿using Service.Models;
+using System.Collections.Generic;
+
+namespace Service
+{
+    public interface IFulfilmentOrder
+    {
+        /// <summary>
+        /// Returns all orders
+        /// </summary>
+        /// <returns></returns>
+        OrderProduct GetOrders();
+
+        /// <summary>
+        /// Returns all orders matching the status
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<OrderProduct> GetOrdersByStatus(string status);
+    }
+}
